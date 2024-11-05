eb5a856674efc0153318088650793d7118d659f7
Tomas Bjerre <tomas.bjerre85@gmail.com>
2024-11-05 12:31:16 +0100

    feat(gradle): format json and groovy

--- fk-code-standard-gradle-plugin/src/main/java/se/fk/fkcodestandard/gradle/FKCodeStandardPlugin.java
+++ fk-code-standard-gradle-plugin/src/main/java/se/fk/fkcodestandard/gradle/FKCodeStandardPlugin.java
@@ -40,6 +40,19 @@ public class FKCodeStandardPlugin implements Plugin<Project> {
 //			eclipse.withP2Mirrors(Map.of("https://download.eclipse.org/eclipse/updates/" + eclipseVersion + "/",
 //					"https://repo1.maven.org/maven2/Eclipse_" + eclipseVersion + "/"));
 		});
+
+		spotlessExtension.groovyGradle(spotlessGroovyExtension -> {
+			spotlessJavaExtension.target('*.gradle', 'src/main/**/*.gradle');
+			spotlessJavaExtension.greclipse()
+		})
+
+		spotlessExtension.json(jsonExtension -> {
+			jsonExtension.target('*.json', 'src/main/**/*.json', 'src/test/**/*.json', 'mocks/**/*.json');
+			jsonExtension.gson()
+				.indentWithSpaces(2)
+				.version('2.8.1')
+		})
+
 		project.getExtensions().add("fkSpotlessExtension", spotlessExtension);
 
 		project.getTasksByName("check", true).forEach(checkTask -> {
