e77930373700fb4bd10ef6b31b9e21bb60763091
Tomas Bjerre <tomas.bjerre85@gmail.com>
2025-10-10 06:04:15 +0200

    fix: avoid releasing example

--- build-and-test.sh
+++ build-and-test.sh
@@ -14,9 +14,9 @@ cd $SCRIPT_DIR
 # Test Maven plugin
 #
 cd $SCRIPT_DIR/fk-code-standard-maven-plugin-example
-./mvnw versions:update-properties -DallowSnapshots=true
-(./mvnw se.fk.codestandard:fk-code-standard-maven-plugin:spotlessCheck && exit 1) || echo "Should fail invalid format"
-(./mvnw package -Pskip-automatic-fk-code-standard-apply=true && exit 1) || echo "Should fail invalid format"
-./mvnw verify
-./mvnw se.fk.codestandard:fk-code-standard-maven-plugin:spotlessApply
-./mvnw se.fk.codestandard:fk-code-standard-maven-plugin:spotlessCheck
+./mvnw -P run-tests-only versions:update-properties -DallowSnapshots=true
+(./mvnw -P run-tests-only se.fk.codestandard:fk-code-standard-maven-plugin:spotlessCheck && exit 1) || echo "Should fail invalid format"
+(./mvnw -P run-tests-only package -Pskip-automatic-fk-code-standard-apply=true && exit 1) || echo "Should fail invalid format"
+./mvnw -P run-tests-only verify
+./mvnw -P run-tests-only se.fk.codestandard:fk-code-standard-maven-plugin:spotlessApply
+./mvnw -P run-tests-only se.fk.codestandard:fk-code-standard-maven-plugin:spotlessCheck
